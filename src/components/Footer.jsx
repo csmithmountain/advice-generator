a124fc28fe03b3e1852fc284369e1764fbdd33c4
csmithmountain <c.savela.smedberg@gmail.com>
2024-04-07 17:55:55 +0200

    removed comments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,20 @@ function Footer() {
         Challenge by{' '}
         <a
           href="https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db/hub"
-          target="_blank" // This line needs correction
-          rel="noopener noreferrer" // Added rel attribute for security
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Frontend Mentor
         </a>
         . Coded by{' '}
-        <a href="https://github.com/csmithmountain">c.smithmountain</a>.
+        <a
+          href="https://github.com/csmithmountain"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          c.smithmountain
+        </a>
+        .
       </div>
     </div>
   );
